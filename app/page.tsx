3cc1e0467524653311e0d074e527933df666691e
Saqib Ejaz <47318304+saqib2598@users.noreply.github.com>
2024-01-22 13:55:46 -0500

    Completed FE with next js and tailwind (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,55 @@
-import Navbar from "@/components/header/navbar";
-import upload_icon from "@/public/images/upload_icon.svg";
+'use client'
 
-export default function Home() {
+import NftForm from "@/components/forms/nft-form";
+import Navbar from "@/components/header/navbar";
+import ConnectWallets from "@/components/wallets/connect-wallets";
+import { useState } from "react";
+import upload_icon from "@/public/images/upload_icon.svg";
+import Footer from "@/components/footer/footer";
+
+const Home = () => {
+
+  const [showWallets, setShowWallets] = useState(false);
+  const [wallets, setWallets] = useState([
+    {id: 1, name: 'Metamask'},
+    {id: 2, name: 'Portis'},
+    {id: 3, name: 'Torus'},
+    {id: 4, name: 'Walletlink'}
+  ]);
+
   return (
     <div>
-      <Navbar />
+      {/* Navbar */}
+      <Navbar
+        setShowWallets={setShowWallets}
+      />
+      {/* Jumbotron */}
       <div className="flex items-center justify-center mt-5 mx-auto w-[1140px] h-[216.14px] top-[113.94px] left-[150px] rounded-lg border border-gray-300 frosted-glass">
         <div className="text-center">
           <h1 className="font-cinzel text-4xl gradient-text">MINT NEW NFT</h1>
           <p className="text-white font-open-sans text-base font-normal leading-[154.023%] opacity-70 w-[560px] h-[52.26px] flex-shrink-0 mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sem tortor quis amet scelerisque vivamus egestas. </p>
         </div>
       </div>
-
-      <form className="flex justify-center mt-20">
-        <label className="w-[544.158px] h-[94px] flex-shrink-0 border rounded-lg border-dashed border-gray-400 bg-gray-700 cursor-pointer text-white text-center flex items-center justify-center">
-          <input
-            type="file"
-            className="hidden"
-            // Add any additional attributes or event handlers for the file input here
+      
+      {/* NFT form */}
+      <div className="flex justify-center items-center">
+        <NftForm />
+      </div>
+      
+      {/* Right rail */}
+      {
+        showWallets && (
+          <ConnectWallets
+            wallets={wallets}
+            show={showWallets}
+            setShow={setShowWallets}
           />
-          <img src={`${upload_icon.src}`} alt="Image" className="mr-2" />
-          <span className="w-118 h-26 top-[443.66px] left-[670px] font-open-sans text-base font-normal leading-26 tracking-normal text-left">Upload Image</span>
-          {/* <p className="text-sm">File type</p> */}
-        </label>
-      </form>
+        )
+      }
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
 
+export default Home;
--- components/header/navbar.tsx
+++ components/header/navbar.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import logo from '../../public/images/logo.svg';
+import wallet from '../../public/images/wallet.svg';
 
-const Navbar: React.FC = () => {
+interface Props {
+  setShowWallets: (showWallets: boolean) => void;
+}
+
+const Navbar = ({setShowWallets}: Props) => {
   return (
     <nav className="w-[1140px] h-[70px] flex items-center justify-between mx-auto">
       <div className="flex items-center">
         <img src={`${logo.src}`} alt="Logo" className="w-[199px] h-[70px]" />
       </div>
-      <div className="w-[159px] h-[25px]">
+      <div className="w-[190px] h-[25px] flex items-center">
         <span className="font-open-sans text-base font-semibold leading-[25px] tracking-normal text-left">
           Explore Marketplace
         </span>
-      </div>
+        <img onClick={() => setShowWallets(true)} src={`${wallet.src}`} alt="Image" className="ml-2" />
+    </div>
     </nav>
   );
 };
